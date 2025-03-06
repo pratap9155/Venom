9067a5b65ef9cc423627bc3a3a2a6bd7c8e93700
VENOM <venommusic91@gmail.com>
2025-03-06 12:56:14 +0530

    .

--- SUHANIMUSIC/platforms/Youtube.py
+++ SUHANIMUSIC/platforms/Youtube.py
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "AARVIMUSIC/assets/cookies.txt"
+cookies_file = "AARVIMUSIC/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
