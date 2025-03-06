412cd1fa526344ed455c4e86bea5d9b0540b8221
VENOM <venommusic91@gmail.com>
2025-03-06 22:33:04 +0530

    Update Youtube.py

--- SUHANIMUSIC/platforms/Youtube.py
+++ SUHANIMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AARVIMUSIC.utils.database import is_on_off
-from AARVIMUSIC.utils.formatters import time_to_seconds
+from SUHANIMUSIC.utils.database import is_on_off
+from SUHANIMUSIC.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "AARVIMUSIC/cookies.txt"
+cookies_file = "SUHANIMUSIC/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
